126893e332805d99cd8d7596a8a5768b0572fac7
SAMI RASHED <srashed@uci.edu>
2023-01-09 19:56:06 -0800

    final1

--- src/about/HeaderLeft.js
+++ src/about/HeaderLeft.js
@@ -10,10 +10,8 @@ function HeaderLeft({ left, right, content }) {
             fontSize: { xs: 54, sm: 74, md: 94 },
             fontFamily: "Raleway",
             fontWeight: "bold",
-            // backgroundImage: "url(cool-background3.png)",
-            // backgroundSize: "cover",
             paddingLeft: 4,
-            color: "red",
+            color: "#284B63",
             textShadow: "2px 2px 2px lightgrey"
           }}
         >
@@ -34,7 +32,13 @@ function HeaderLeft({ left, right, content }) {
         <Grid
           item
           xs={9}
-          sx={{ height: 10, backgroundColor: "grey" }}
+          sx={{
+            height: 10,
+            backgroundColor: "#3C6E71",
+            borderTopRightRadius: 3,
+            borderBottomRightRadius: 3,
+            boxShadow: 5,
+          }}
         ></Grid>
         <Grid item xs={3}></Grid>
       </Grid>
